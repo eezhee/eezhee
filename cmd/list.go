5ff69112e6d2461ac704f95d6088471b29c9c939
Athir Nuaimi <athir@ruksack.com>
2020-09-25 07:20:38 -0400

    early version of build command that creates a VM

--- cmd/list.go
+++ cmd/list.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List the apps you have running",
+	Long:  `All software has versions. This is Eezhee's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("listing apps... done")
+
+		// get a list of VMs running on DO
+		// see if have the 'eezhee' tag
+
+	},
+}
