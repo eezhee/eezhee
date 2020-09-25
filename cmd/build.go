c8051234b6086c87b24e2c5d9db6cc183e26243e
Athir Nuaimi <athir@ruksack.com>
2020-09-25 07:21:04 -0400

    early version of build command that creates a VM

--- cmd/build.go
+++ cmd/build.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -18,18 +21,44 @@ var buildCmd = &cobra.Command{
 	Short: "Print the version number of Eezhee",
 	Long:  `All software has versions. This is Eezhee's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("building...done")
-
 		buildVM()
-
 	},
+}
+
+// check if doctl is install and we have an auth token
+func validateRequirements() (bool, error) {
+
+	// is doctl installed
+	cmd := exec.Command("which", "doctl")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return false, errors.New("doctl is not installed")
+	}
+
+	// has user authenticated
+	// should list at least one item, normally is `default` context
+	cmd = exec.Command("doctl", "auth", "list")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		return false, errors.New("doctl is not logged in.  Use 'doctl auth init'")
+	}
+	// fmt.Println(string(stdoutStderr))
+
+	return true, nil
 }
 
 func buildVM() bool {
 
 	// is there a deploy state file
-	// yes, error out
-	//
+
+	haveRequirements, err := validateRequirements()
+	if !haveRequirements {
+		fmt.Println(err)
+		return false
+	}
+
+	vmName, _ := buildClusterName()
+	fmt.Println(vmName)
 
 	sshFingerprint, err := getSSHFingerprint()
 	if err != nil {
@@ -38,21 +67,82 @@ func buildVM() bool {
 	}
 	fmt.Println(sshFingerprint)
 
-	uploaded := checkSSHKeyUploaded(sshFingerprint)
+	uploaded, err := checkSSHKeyUploaded(sshFingerprint)
 	if !uploaded {
+		fmt.Println(err)
 		return false
 	}
 
 	// get directory name
-	// vmName := "eezhee-test"
-	// imageName := "ubuntu-20-04-x64"
-	// vmSize := "s-1vcpu-1gb"
-	// region := "tor1"
+	imageName := "ubuntu-20-04-x64"
+	vmSize := "s-1vcpu-1gb"
+	region := "tor1"
 
-	// // doctl compute droplet create
-	// RESULT = `doctl compute droplet create $VM_NAME --image $VM_IMAGE --size $VM_SIZE --region $VM_REGION --ssh-keys $DO_FINGERPRINT -o json`
+	// time to create the VM
+	cmd := exec.Command("doctl", "compute", "droplet", "create", vmName, "--image", imageName, "--size", vmSize, "--region", region, "--ssh-keys", sshFingerprint, "-o", "json")
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	fmt.Println(stdoutStderr)
+
+	// parse the output
+	// get the ID
+	// save the ID
 
 	return true
+}
+
+// figure out what to call k3s cluster
+// based on combo of app name and git branch (if not master
+// eg webapp, webapp-staging, webapp-newFeatureBranch)
+func buildClusterName() (string, error) {
+
+	clusterName := ""
+
+	appName, _ := getCurrentDir()
+	branchName, err := getGitBranchName()
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	if len(branchName) > 0 {
+		clusterName = appName + "-" + branchName
+	} else {
+		clusterName = appName + branchName
+	}
+	// fmt.Println(clusterName)
+
+	return clusterName, nil
+}
+
+func getCurrentDir() (string, error) {
+
+	fullDirPath, _ := os.Getwd()
+	fields := strings.Split(fullDirPath, "/")
+	currentDir := fields[len(fields)-1]
+
+	return currentDir, nil
+}
+
+func getGitBranchName() (string, error) {
+
+	// see if there is a .git subdir
+	command := "git"
+	cmd := exec.Command(command, "rev-parse", "--abbrev-ref", "HEAD")
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	branchName := string(stdoutStderr)
+	branchName = strings.TrimSpace(branchName)
+
+	if strings.Compare(branchName, "master") == 0 {
+		return "", nil
+	}
+
+	return branchName, nil
 }
 
 // use ssh-keygen to get the fingerprint for a ssh key
@@ -84,16 +174,34 @@ func getSSHFingerprint() (string, error) {
 	return fingerprint, nil
 }
 
+type digitalOceanSSHKeys struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Fingerprint string `json:"fingerprint"`
+	PublicKey   string `json:"public_key"`
+}
+
 // check if ssh key already uploaded to DigitalOcean
-func checkSSHKeyUploaded(fingerprint string) bool {
+func checkSSHKeyUploaded(fingerprint string) (bool, error) {
 
-	// TODO: need to check if has been uploaded to DO
-	// DO_SSH_KEYS=`doctl compute ssh-key list`
-	// if [[ ${DO_SSH_KEYS} != *${DO_FINGERPRINT}* ]]; then
-	// 	echo "Need to upload SSH key to DO"
-	// 	# new_key_name should be same as $HOME
-	// 	# doctl import new_key_name --public-key-file ~/.ssh/id_rsa.pub
-	// fi
+	var sshKeys []digitalOceanSSHKeys
 
-	return true
+	// get list of sshkeys DO knows about
+	cmd := exec.Command("doctl", "compute", "ssh-key", "list", "-o", "json")
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		return false, err
+	}
+
+	json.Unmarshal([]byte(stdoutStderr), &sshKeys)
+
+	// go through each key and see if it matches what is on this machine
+	for i := 0; i < len(sshKeys); i++ {
+		if strings.Compare(fingerprint, sshKeys[i].Fingerprint) == 0 {
+			// fmt.Println("found ssh key")
+			return true, nil
+		}
+	}
+
+	return false, errors.New("ssh key not available.  you need to upload it to DO using the web console")
 }
