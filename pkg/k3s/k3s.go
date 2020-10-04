7d67b1d84474ae10575cf05ed54ee9a5a3857342
Athir Nuaimi <athir@ruksack.com>
2020-10-04 19:19:51 -0400

    refactor k3s package into struct with methods

--- pkg/k3s/k3s.go
+++ pkg/k3s/k3s.go
@@ -1,6 +1,7 @@
 package k3s
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os/exec"
@@ -28,31 +29,108 @@ import (
 
 // Version of k3s
 type Version struct {
-	Track   string
+	Track   string // ie. 1.19
 	Major   string
 	Minor   string
 	Release string
 }
 
-// checkRequirements will see if k3sup is already installed
-func checkRequirements() bool {
+// Manager will handle installation of k3s
+type Manager struct {
+	k3sReleases map[string][]string // list of available k3s versions, groups by track (ie 1.19)
+
+	// we should cache versions. only update if call GetVersions
+	// maybe we do the 'latest' trick to see if changed
+	// http://github.com/rancher/k3s/releases/latest
+}
+
+// NewManager will create a new k3s manager
+func NewManager() *Manager {
+	m := new(Manager)
+
+	// pre-fetch list of versions we can install
+	m.GetVersions()
+
+	return m
+}
+
+// parse a given version string into its components
+func parseVersion(versionStr string) (version Version, err error) {
+
+	// v1.19.2
+	versionParts := strings.Split(versionStr, ".")
+
+	version.Major = versionParts[0]
+	version.Major = version.Major[1:len(version.Major)]
+	version.Minor = versionParts[1]
+	version.Release = versionParts[2]
+	version.Track = version.Major + "." + version.Minor
+
+	return version, err
+}
+
+// GetVersions of K3S that are available
+func (m *Manager) GetVersions() (map[string][]string, error) {
+
+	m.k3sReleases = make(map[string][]string)
+
+	releases, err := github.GetRepoReleases("rancher", "k3s")
+	if err != nil {
+		return nil, err
+	}
+
+	// go through releases and filter out any non-final (non-RC) releases
+	for _, release := range releases {
+
+		// check tag name.  note, can't use 'releaseName' as is blank for older releases (pre-2020)
+		tagName := release.TagName
+		// releaseName := release.Name  		// older releases have this blank
+
+		// parse release name
+		fields := strings.Split(tagName, "+")
+		// releaseParts[1] should always be 'k3s1'
+		releaseParts := strings.Split(fields[0], "-")
+		if len(releaseParts) == 1 {
+			// only want final releases
+			fullVersion := releaseParts[0]
+			version, err := parseVersion(fullVersion)
+			if err != nil {
+				// skip this version
+				fmt.Printf("could not parse version %s\n", fullVersion)
+			}
+
+			// sort into streams   1.16, 1.17, etc
+			// ignore version before 1.16
+			// note versions in each track will be in desending order (ie 1.19.2, 1.19.1)
+			if strings.Compare(version.Track, "1.16") >= 0 {
+				m.k3sReleases[version.Track] = append(m.k3sReleases[version.Track], fullVersion)
+			}
+
+		} else {
+			// ignore non-final releases
+			// fmt.Println("ignoring", releaseParts[0], releaseParts[1])
+		}
+	}
+
+	return m.k3sReleases, nil
+}
+
+// CheckRequirements will make sure required components are installed
+func (m *Manager) CheckRequirements() (bool, error) {
 
 	if runtime.GOOS == "windows" {
-		fmt.Println("tool does not support windows yet")
-		return false
+		return false, errors.New("tool does not support windows yet")
 	}
 
 	// is brew installed?
 	cmd := exec.Command("which", "brew")
-	stdoutStderr, err := cmd.CombinedOutput()
+	_, err := cmd.CombinedOutput()
 	if err != nil {
 		// brew not installed
-		fmt.Println(string(stdoutStderr))
-		fmt.Println("brew not installed. Check https://brew.sh for instructions")
-		return false
+		return false, errors.New("brew not installed. check https://brew.sh for instructions")
 	}
 
-	return true
+	return true, nil
 }
 
 // setupK3sup will make sure k3sup on machine
@@ -141,10 +219,12 @@ func setupK3sup() bool {
 }
 
 // Install k3s using k3sup
-func Install() bool {
+func (m *Manager) Install() bool {
 
 	// make sure we have brew
-	if !checkRequirements() {
+	_, err := m.CheckRequirements()
+	if err != nil {
+		fmt.Println(err)
 		return false
 	}
 
@@ -159,64 +239,4 @@ func Install() bool {
 	// need ip, name of ssh key, user
 
 	return true
-}
-
-func parseVersion(versionStr string) (version Version, err error) {
-
-	// v1.19.2
-	versionParts := strings.Split(versionStr, ".")
-
-	version.Major = versionParts[0]
-	version.Major = version.Major[1:len(version.Major)]
-	version.Minor = versionParts[1]
-	version.Release = versionParts[2]
-	version.Track = version.Major + "." + version.Minor
-
-	return version, err
-}
-
-// GetVersions of K3S that are available
-func GetVersions() (map[string][]string, error) {
-
-	k3sReleases := make(map[string][]string)
-
-	releases, err := github.GetRepoReleases("rancher", "k3s")
-	if err != nil {
-		return nil, err
-	}
-
-	// go through releases and filter out any non-final (non-RC) releases
-	for _, release := range releases {
-
-		// check tag name.  note, can't use 'releaseName' as is blank for older releases (pre-2020)
-		tagName := release.TagName
-		// releaseName := release.Name  		// older releases have this blank
-
-		// parse release name
-		fields := strings.Split(tagName, "+")
-		// releaseParts[1] should always be 'k3s1'
-		releaseParts := strings.Split(fields[0], "-")
-		if len(releaseParts) == 1 {
-			// only want final releases
-			fullVersion := releaseParts[0]
-			version, err := parseVersion(fullVersion)
-			if err != nil {
-				// skip this version
-				fmt.Printf("could not parse version %s\n", fullVersion)
-			}
-
-			// sort into streams   1.16, 1.17, etc
-			// ignore version before 1.16
-			// note versions in each track will be in desending order (ie 1.19.2, 1.19.1)
-			if strings.Compare(version.Track, "1.16") >= 0 {
-				k3sReleases[version.Track] = append(k3sReleases[version.Track], fullVersion)
-			}
-
-		} else {
-			// ignore non-final releases
-			// fmt.Println("ignoring", releaseParts[0], releaseParts[1])
-		}
-	}
-
-	return k3sReleases, nil
 }
