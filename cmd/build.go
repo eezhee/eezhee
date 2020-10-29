c055d61a9a8b86774e9945147514a9c3cb905a93
Athir Nuaimi <athir@nuaimi.com>
2020-10-28 21:55:19 -0400

    linode support added

--- cmd/build.go
+++ cmd/build.go
@@ -64,11 +64,13 @@ func buildCluster() error {
 		}
 	}
 
-	// set name for cluster - default to project & branch name
+	// make sure we have a name for the cluster
+	// if not set, create a name
 	if len(deployConfig.Name) == 0 {
 		deployConfig.Name, _ = buildClusterName()
 	}
 
+	// TODO - need public key as well - create ssh key struct
 	// get ssh key we will use to login to new VM
 	sshFingerprint, err := getSSHFingerprint()
 	if err != nil {
@@ -76,10 +78,14 @@ func buildCluster() error {
 	}
 	deployConfig.SSHFingerprint = sshFingerprint
 
-	// see which cloud to create the VM on
+	// does config specify which cloud to use
+	// TODO: if not, use one that we have credentials for
 	if len(deployConfig.Cloud) == 0 {
 		deployConfig.Cloud = "digitalocean"
 	}
+
+	// make sure we have a valid cloud
+	// TODO: make sure we have credentials for given cloud
 	switch deployConfig.Cloud {
 	case "digitalocean":
 	case "linode":
@@ -91,15 +97,15 @@ func buildCluster() error {
 	}
 	fmt.Println("deplying to", deployConfig.Cloud)
 
-	// check what release of k3s should be used
-	// default to stable if not set
+	// has a release of k3s been specified?
+	// if not, use latest stable release
 	if len(deployConfig.K3sVersion) == 0 {
 		deployConfig.K3sVersion = "stable"
 	}
 
-	// make sure it's valid and if its a pinned release or a channel,
-	// convert to actual release to be installed
-	// needs to be 'stable', 'latest', validChannelName (v1.19) or validReleaseName (v1.19.3)
+	// we're pretty flexibly in how release is specified.
+	// could be 'stable', 'latest', validChannelName (v1.19) or validReleaseName (v1.19.3)
+	// as a result, we need to translate it to exactly which release to install
 	k3sManager := k3s.NewManager()
 	release, err := k3sManager.Releases.Translate(deployConfig.K3sVersion)
 	if err != nil {
@@ -108,26 +114,34 @@ func buildCluster() error {
 	deployConfig.K3sVersion = release
 
 	// ok validation completed, time to get building
+
+	// create an instance of the VM manager which does building
 	var vmManager core.VMManager
 
 	switch deployConfig.Cloud {
 	case "digitalocean":
-		// make sure we can talk to DigitalOcean
 		vmManager = digitalocean.NewManager(appConfig.DigitalOceanAPIKey)
-
+		if vmManager == nil {
+			return errors.New("could not create digitalocean client")
+		}
 	case "linode":
 		vmManager = linode.NewManager(appConfig.LinodeAPIKey)
 		if vmManager == nil {
 			return errors.New("could not create linode client")
 		}
+	default:
+		// should never get here (but lets play it safe)
+		return errors.New("invalid cloud type")
 	}
 
+	// TODO: for DO, should upload it if not there yet
 	// make sure this ssh key is loaded into cloud platform
 	uploaded, err := vmManager.IsSSHKeyUploaded(sshFingerprint)
 	if !uploaded {
 		return err
 	}
 
+	// TODO: need to valide if it a valid region for given cloud
 	// set rest of details for new VM
 	if len(deployConfig.Region) == 0 {
 		fmt.Println("selecting closest region")
@@ -138,6 +152,8 @@ func buildCluster() error {
 		fmt.Println(deployConfig.Region, "is closest")
 	}
 
+	// TODO - allow config to specify size/type
+	// TODO - translate generic size/type to provider specific
 	var imageName string
 	switch deployConfig.Cloud {
 	case "digitalocean":
@@ -165,6 +181,8 @@ func buildCluster() error {
 	status := vmInfo.Status
 
 	// TODO - this is not true for linode
+	// TOOD - should DO.CreateVM not return until VM 'active' and there is an IP?
+
 	// see if vm ready.  if not need to wait as don't have IP yet
 	for strings.Compare(status, "active") != 0 {
 
@@ -194,11 +212,14 @@ func buildCluster() error {
 	deployState.Region = vmInfo.Region.Slug
 	deployState.Size = vmInfo.Size.Slug
 	deployState.IP = vmPublicIP
+	// TODO save public key
 	deployState.SSHFingerprint = deployConfig.SSHFingerprint
 	err = deployState.Save()
 	if err != nil {
 		return err
 	}
+
+	// TODO: refactor this into another function
 
 	// install k3s on the VM
 	k3sVersion := deployConfig.K3sVersion
@@ -213,6 +234,8 @@ func buildCluster() error {
 	if err != nil {
 		return err
 	}
+
+	// TODO: what about installing ingress?
 
 	return nil
 }
--- pkg/linode/linode.go
+++ pkg/linode/linode.go
@@ -2,7 +2,6 @@ package linode
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
@@ -261,295 +260,295 @@ func (m *Manager) DeleteVM(ID int) error {
 // used to develop and test code
 //
 // sample has code on how to call various linode apis
-func (m *Manager) sample() (vmInfo core.VMInfo, err error) {
+// func (m *Manager) sample() (vmInfo core.VMInfo, err error) {
 
-	testProfile := false
-	if testProfile {
-		profile, err := m.api.GetProfile(context.Background())
-		if err != nil {
-			return vmInfo, err
-		}
-		fmt.Println(profile.AuthorizedKeys)
-	}
+// 	testProfile := false
+// 	if testProfile {
+// 		profile, err := m.api.GetProfile(context.Background())
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		fmt.Println(profile.AuthorizedKeys)
+// 	}
 
-	testImages := false
-	if testImages {
-		// get list of VM types (~27 variations)
-		images, err := m.api.ListImages(context.Background(), nil)
-		if err != nil {
-			return vmInfo, nil
-		}
-		for _, image := range images {
-			fmt.Println("image:", image.Label, " ", image.ID, " ", image.Description)
-		}
+// 	testImages := false
+// 	if testImages {
+// 		// get list of VM types (~27 variations)
+// 		images, err := m.api.ListImages(context.Background(), nil)
+// 		if err != nil {
+// 			return vmInfo, nil
+// 		}
+// 		for _, image := range images {
+// 			fmt.Println("image:", image.Label, " ", image.ID, " ", image.Description)
+// 		}
 
-		desiredImage := images[0].ID
-		image, err := m.api.GetImage(context.Background(), desiredImage)
-		if err != nil {
-			return vmInfo, err
-		}
-		fmt.Println(image.Type)
-	}
+// 		desiredImage := images[0].ID
+// 		image, err := m.api.GetImage(context.Background(), desiredImage)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		fmt.Println(image.Type)
+// 	}
 
-	testTypes := true
-	if testTypes {
-		// get list of VM types (~27 variations)
-		vmTypes, err := m.api.ListTypes(context.Background(), nil)
-		if err != nil {
-			return vmInfo, err
-		}
-		for _, vmInfo := range vmTypes {
-			fmt.Println("type:", vmInfo.ID, " ", vmInfo.Label)
-		}
+// 	testTypes := true
+// 	if testTypes {
+// 		// get list of VM types (~27 variations)
+// 		vmTypes, err := m.api.ListTypes(context.Background(), nil)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		for _, vmInfo := range vmTypes {
+// 			fmt.Println("type:", vmInfo.ID, " ", vmInfo.Label)
+// 		}
 
-		desiredInstance := vmTypes[0].ID
-		instanceType, err := m.api.GetType(context.Background(), desiredInstance)
-		if err != nil {
-			return vmInfo, err
-		}
-		fmt.Println(instanceType.ID)
-	}
+// 		desiredInstance := vmTypes[0].ID
+// 		instanceType, err := m.api.GetType(context.Background(), desiredInstance)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		fmt.Println(instanceType.ID)
+// 	}
 
-	testTags := false
-	if testTags {
+// 	testTags := false
+// 	if testTags {
 
-		tagExists := false
+// 		tagExists := false
 
-		// get list of existing tags
-		tags, err := m.api.ListTags(context.Background(), nil)
-		if err != nil {
-			return vmInfo, err
-		}
+// 		// get list of existing tags
+// 		tags, err := m.api.ListTags(context.Background(), nil)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
 
-		// does our tag already exist?
-		for _, tag := range tags {
-			fmt.Println(tag.Label)
-			if strings.Compare(tag.Label, "eezhee") == 0 {
-				tagExists = true
-			}
-		}
+// 		// does our tag already exist?
+// 		for _, tag := range tags {
+// 			fmt.Println(tag.Label)
+// 			if strings.Compare(tag.Label, "eezhee") == 0 {
+// 				tagExists = true
+// 			}
+// 		}
 
-		if tagExists {
-			err = m.api.DeleteTag(context.Background(), "eezhee")
-			if err != nil {
-				return vmInfo, err
-			}
-			tagExists = false
-		}
-		// if not, need to add
-		if !tagExists {
-			// create the eezhee tag so we can use when we create objects
-			tagCreateOpts := linodego.TagCreateOptions{
-				Label: "eezhee",
-			}
-			newTag, err := m.api.CreateTag(context.Background(), tagCreateOpts)
-			if err != nil {
-				return vmInfo, err
-			}
-			fmt.Println("tag:", newTag.Label)
-		}
+// 		if tagExists {
+// 			err = m.api.DeleteTag(context.Background(), "eezhee")
+// 			if err != nil {
+// 				return vmInfo, err
+// 			}
+// 			tagExists = false
+// 		}
+// 		// if not, need to add
+// 		if !tagExists {
+// 			// create the eezhee tag so we can use when we create objects
+// 			tagCreateOpts := linodego.TagCreateOptions{
+// 				Label: "eezhee",
+// 			}
+// 			newTag, err := m.api.CreateTag(context.Background(), tagCreateOpts)
+// 			if err != nil {
+// 				return vmInfo, err
+// 			}
+// 			fmt.Println("tag:", newTag.Label)
+// 		}
 
-	}
+// 	}
 
-	testRegions := false
-	if testRegions {
-		regions, _ := m.api.ListRegions(context.Background(), nil)
-		for _, region := range regions {
-			fmt.Println(region.Country, " ", region.ID)
-		}
-		fmt.Println("")
+// 	testRegions := false
+// 	if testRegions {
+// 		regions, _ := m.api.ListRegions(context.Background(), nil)
+// 		for _, region := range regions {
+// 			fmt.Println(region.Country, " ", region.ID)
+// 		}
+// 		fmt.Println("")
 
-	}
+// 	}
 
-	testDomains := false
-	if testDomains {
-		// createDomainOpts := linodego.DomainCreateOptions{
-		// 	Domain:   "testing-eezhee.com",
-		// 	Type:     linodego.DomainTypeMaster,
-		// 	SOAEmail: "support@eezhee.com",
-		// }
-		// createDomainOpts.Tags = append(createDomainOpts.Tags, "eezhee")
-		// newDomain, err := m.api.CreateDomain(context.Background(), createDomainOpts)
-		// if err != nil {
-		// 	return vmInfo, err
-		// }
-		// fmt.Println(newDomain.ID)
+// 	testDomains := false
+// 	if testDomains {
+// 		// createDomainOpts := linodego.DomainCreateOptions{
+// 		// 	Domain:   "testing-eezhee.com",
+// 		// 	Type:     linodego.DomainTypeMaster,
+// 		// 	SOAEmail: "support@eezhee.com",
+// 		// }
+// 		// createDomainOpts.Tags = append(createDomainOpts.Tags, "eezhee")
+// 		// newDomain, err := m.api.CreateDomain(context.Background(), createDomainOpts)
+// 		// if err != nil {
+// 		// 	return vmInfo, err
+// 		// }
+// 		// fmt.Println(newDomain.ID)
 
-		// does the domain that we care about exist?
-		targetDomain := "testing-eezhee.com"
-		domainExists := false
+// 		// does the domain that we care about exist?
+// 		targetDomain := "testing-eezhee.com"
+// 		domainExists := false
 
-		// list any domains linode is hosting
-		domains, err := m.api.ListDomains(context.Background(), nil)
-		for _, domain := range domains {
-			if strings.Compare(targetDomain, domain.Domain) == 0 {
-				domainExists = true
-				break
-			}
-			fmt.Println(domain.ID, " ", domain.Domain)
-		}
-		if !domainExists {
-			// since don't have domain hosted in linode, can't add any records
-			return vmInfo, err
-		}
+// 		// list any domains linode is hosting
+// 		domains, err := m.api.ListDomains(context.Background(), nil)
+// 		for _, domain := range domains {
+// 			if strings.Compare(targetDomain, domain.Domain) == 0 {
+// 				domainExists = true
+// 				break
+// 			}
+// 			fmt.Println(domain.ID, " ", domain.Domain)
+// 		}
+// 		if !domainExists {
+// 			// since don't have domain hosted in linode, can't add any records
+// 			return vmInfo, err
+// 		}
 
-		// get details
-		// this really isn't needed
-		domain := domains[0]
-		domainDetails, err := m.api.GetDomain(context.Background(), domain.ID)
-		if err != nil {
-			return vmInfo, err
-		}
-		fmt.Println(domainDetails.Domain)
+// 		// get details
+// 		// this really isn't needed
+// 		domain := domains[0]
+// 		domainDetails, err := m.api.GetDomain(context.Background(), domain.ID)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		fmt.Println(domainDetails.Domain)
 
-		// records
-		domainRecordExists := false
+// 		// records
+// 		domainRecordExists := false
 
-		// see if A record already exists for host
-		filter, _ := json.Marshal(map[string]interface{}{"name": "www"})
-		listOpts := linodego.NewListOptions(0, string(filter))
-		records, err := m.api.ListDomainRecords(context.Background(), domainDetails.ID, listOpts)
-		if err != nil {
-			return vmInfo, err
-		}
-		for _, record := range records {
-			// type = A and name = www
-			if record.Type == "A" && (strings.Compare("www", record.Name)) == 0 {
-				domainRecordExists = true
-				break
-			}
+// 		// see if A record already exists for host
+// 		filter, _ := json.Marshal(map[string]interface{}{"name": "www"})
+// 		listOpts := linodego.NewListOptions(0, string(filter))
+// 		records, err := m.api.ListDomainRecords(context.Background(), domainDetails.ID, listOpts)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		for _, record := range records {
+// 			// type = A and name = www
+// 			if record.Type == "A" && (strings.Compare("www", record.Name)) == 0 {
+// 				domainRecordExists = true
+// 				break
+// 			}
 
-			fmt.Println(record.Name)
-		}
+// 			fmt.Println(record.Name)
+// 		}
 
-		if domainRecordExists {
-			// update the record
-			recUpdateOpts := linodego.DomainRecordUpdateOptions{
-				Target: "192.168.1.1",
-			}
-			domainRecordID := records[0].ID
-			updatedRecord, err := m.api.UpdateDomainRecord(context.Background(), domainDetails.ID, domainRecordID, recUpdateOpts)
-			if err != nil {
-				return vmInfo, nil
-			}
-			fmt.Println(updatedRecord.Target)
-		} else {
-			// create the record
-			recCreateOpts := linodego.DomainRecordCreateOptions{
-				Type: linodego.RecordTypeA,
-				Name: "www",
-				// TTLSec: 60,
-				Target: "127.0.0.1",
-			}
-			// recCreateOpts.Tag = strCopy("eezhee") // can't assign a constant
-			newRecord, err := m.api.CreateDomainRecord(context.Background(), domainDetails.ID, recCreateOpts)
-			if err != nil {
-				return vmInfo, err
-			}
-			fmt.Println(newRecord.ID)
-		}
+// 		if domainRecordExists {
+// 			// update the record
+// 			recUpdateOpts := linodego.DomainRecordUpdateOptions{
+// 				Target: "192.168.1.1",
+// 			}
+// 			domainRecordID := records[0].ID
+// 			updatedRecord, err := m.api.UpdateDomainRecord(context.Background(), domainDetails.ID, domainRecordID, recUpdateOpts)
+// 			if err != nil {
+// 				return vmInfo, nil
+// 			}
+// 			fmt.Println(updatedRecord.Target)
+// 		} else {
+// 			// create the record
+// 			recCreateOpts := linodego.DomainRecordCreateOptions{
+// 				Type: linodego.RecordTypeA,
+// 				Name: "www",
+// 				// TTLSec: 60,
+// 				Target: "127.0.0.1",
+// 			}
+// 			// recCreateOpts.Tag = strCopy("eezhee") // can't assign a constant
+// 			newRecord, err := m.api.CreateDomainRecord(context.Background(), domainDetails.ID, recCreateOpts)
+// 			if err != nil {
+// 				return vmInfo, err
+// 			}
+// 			fmt.Println(newRecord.ID)
+// 		}
 
-		// err = m.api.DeleteDomainRecord(context.Background(), domainDetails.ID, domainDetails.ID)
-		// if err != nil {
-		// 	// continue with domain deletion
-		// }
+// 		// err = m.api.DeleteDomainRecord(context.Background(), domainDetails.ID, domainDetails.ID)
+// 		// if err != nil {
+// 		// 	// continue with domain deletion
+// 		// }
 
-		// // remove domains
-		// err = m.api.DeleteDomain(context.Background(), domainDetails.ID)
-		// if err != nil {
-		// 	return vmInfo, err
-		// }
-	}
+// 		// // remove domains
+// 		// err = m.api.DeleteDomain(context.Background(), domainDetails.ID)
+// 		// if err != nil {
+// 		// 	return vmInfo, err
+// 		// }
+// 	}
 
-	testSSHKeys := false
-	if testSSHKeys {
+// 	testSSHKeys := false
+// 	if testSSHKeys {
 
-		// NOTE: linode will allow multiple ssh keys with the same name and same key value
+// 		// NOTE: linode will allow multiple ssh keys with the same name and same key value
 
-		// find the image we want to use
-		// keyExists := false
-		keys, err := m.api.ListSSHKeys(context.Background(), nil)
-		if err != nil {
-			return vmInfo, err
-		}
-		for _, key := range keys {
+// 		// find the image we want to use
+// 		// keyExists := false
+// 		keys, err := m.api.ListSSHKeys(context.Background(), nil)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		for _, key := range keys {
 
-			// get SSHKey, get fingerprint of key and see if matches id_rsa.pub otherwise create it
-			// fingerprint := key.SSHKey
-			fmt.Println(key.ID, " ", key.SSHKey)
-		}
+// 			// get SSHKey, get fingerprint of key and see if matches id_rsa.pub otherwise create it
+// 			// fingerprint := key.SSHKey
+// 			fmt.Println(key.ID, " ", key.SSHKey)
+// 		}
 
-		sshOptions := linodego.SSHKeyCreateOptions{
-			Label:  "athirnuaimi2-eezhee",
-			SSHKey: "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAocQ68fyqU/QZJYrpGrM+tkJDfUPefFPa2Qc+C2BHom3gysv8vqwmFgdVs6Z75rPkUNitpIxUYGPovJbG5pFE6qRNxK3ZHxbk1TSlFBcL8w7jd/jt4IuHwslO4R+hxLG0vzGVFpKSKjAM6yac+q8wOtFU7pKpmrGx9oyClrVQb4mSbCdDazf7/uzXpKMg5mgONbjT6AWSpos2cUDH+VNAQKEnFxKWYjEddCqJnN2kIvtvJUeVhaxYjSVgtiJ7/e0KboDBKRRtO+b4v2TmWmGoRhrPqMo3GazU9aSOAEOMrl3SrxkjmH+eRCUA+1zdvwes8ncVK36FNXzFJ7CxGEAHrw== athir@nuaimi.com",
-		}
-		newKey, err := m.api.CreateSSHKey(context.Background(), sshOptions)
-		if err != nil {
-			return vmInfo, err
-		}
-		fmt.Println(newKey)
+// 		sshOptions := linodego.SSHKeyCreateOptions{
+// 			Label:  "athirnuaimi2-eezhee",
+// 			SSHKey: "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAocQ68fyqU/QZJYrpGrM+tkJDfUPefFPa2Qc+C2BHom3gysv8vqwmFgdVs6Z75rPkUNitpIxUYGPovJbG5pFE6qRNxK3ZHxbk1TSlFBcL8w7jd/jt4IuHwslO4R+hxLG0vzGVFpKSKjAM6yac+q8wOtFU7pKpmrGx9oyClrVQb4mSbCdDazf7/uzXpKMg5mgONbjT6AWSpos2cUDH+VNAQKEnFxKWYjEddCqJnN2kIvtvJUeVhaxYjSVgtiJ7/e0KboDBKRRtO+b4v2TmWmGoRhrPqMo3GazU9aSOAEOMrl3SrxkjmH+eRCUA+1zdvwes8ncVK36FNXzFJ7CxGEAHrw== athir@nuaimi.com",
+// 		}
+// 		newKey, err := m.api.CreateSSHKey(context.Background(), sshOptions)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		fmt.Println(newKey)
 
-	}
+// 	}
 
-	testInstances := true
-	if testInstances {
-		instances, err := m.api.ListInstances(context.Background(), nil)
-		if err != nil {
-			return vmInfo, err
-		}
-		for _, instance := range instances {
-			fmt.Println(instance.ID, " ", instance.Label, " ", instance.IPv4)
-		}
+// 	testInstances := true
+// 	if testInstances {
+// 		instances, err := m.api.ListInstances(context.Background(), nil)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		for _, instance := range instances {
+// 			fmt.Println(instance.ID, " ", instance.Label, " ", instance.IPv4)
+// 		}
 
-		// generate a strong root password.  we will through this away
-		// TODO: should really disable password login for root
-		// TODO: should check if it is actually enabled
-		rootPassword, err := password.Generate(64, 10, 10, false, false)
-		if err != nil {
-			return vmInfo, err
-		}
+// 		// generate a strong root password.  we will through this away
+// 		// TODO: should really disable password login for root
+// 		// TODO: should check if it is actually enabled
+// 		rootPassword, err := password.Generate(64, 10, 10, false, false)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
 
-		createOptions := linodego.InstanceCreateOptions{
-			Region:   "ca-central",
-			Type:     "g6-nanode-1",
-			Label:    "test-001",
-			Image:    "linode/ubuntu20.04",
-			RootPass: rootPassword,
-		}
-		createOptions.AuthorizedKeys = append(createOptions.AuthorizedKeys, "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAocQ68fyqU/QZJYrpGrM+tkJDfUPefFPa2Qc+C2BHom3gysv8vqwmFgdVs6Z75rPkUNitpIxUYGPovJbG5pFE6qRNxK3ZHxbk1TSlFBcL8w7jd/jt4IuHwslO4R+hxLG0vzGVFpKSKjAM6yac+q8wOtFU7pKpmrGx9oyClrVQb4mSbCdDazf7/uzXpKMg5mgONbjT6AWSpos2cUDH+VNAQKEnFxKWYjEddCqJnN2kIvtvJUeVhaxYjSVgtiJ7/e0KboDBKRRtO+b4v2TmWmGoRhrPqMo3GazU9aSOAEOMrl3SrxkjmH+eRCUA+1zdvwes8ncVK36FNXzFJ7CxGEAHrw== athir@nuaimi.com")
-		createOptions.Tags = append(createOptions.Tags, "eezhee")
-		newInstance, err := m.api.CreateInstance(context.Background(), createOptions)
-		if err != nil {
-			return vmInfo, err
-		}
-		fmt.Println(newInstance.ID)
-		fmt.Println(newInstance.Status)
+// 		createOptions := linodego.InstanceCreateOptions{
+// 			Region:   "ca-central",
+// 			Type:     "g6-nanode-1",
+// 			Label:    "test-001",
+// 			Image:    "linode/ubuntu20.04",
+// 			RootPass: rootPassword,
+// 		}
+// 		createOptions.AuthorizedKeys = append(createOptions.AuthorizedKeys, "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAocQ68fyqU/QZJYrpGrM+tkJDfUPefFPa2Qc+C2BHom3gysv8vqwmFgdVs6Z75rPkUNitpIxUYGPovJbG5pFE6qRNxK3ZHxbk1TSlFBcL8w7jd/jt4IuHwslO4R+hxLG0vzGVFpKSKjAM6yac+q8wOtFU7pKpmrGx9oyClrVQb4mSbCdDazf7/uzXpKMg5mgONbjT6AWSpos2cUDH+VNAQKEnFxKWYjEddCqJnN2kIvtvJUeVhaxYjSVgtiJ7/e0KboDBKRRtO+b4v2TmWmGoRhrPqMo3GazU9aSOAEOMrl3SrxkjmH+eRCUA+1zdvwes8ncVK36FNXzFJ7CxGEAHrw== athir@nuaimi.com")
+// 		createOptions.Tags = append(createOptions.Tags, "eezhee")
+// 		newInstance, err := m.api.CreateInstance(context.Background(), createOptions)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
+// 		fmt.Println(newInstance.ID)
+// 		fmt.Println(newInstance.Status)
 
-		// see if vm ready
-		status := newInstance.Status
-		for status != linodego.InstanceRunning {
+// 		// see if vm ready
+// 		status := newInstance.Status
+// 		for status != linodego.InstanceRunning {
 
-			// wait a bit
-			time.Sleep(2 * time.Second)
+// 			// wait a bit
+// 			time.Sleep(2 * time.Second)
 
-			instanceInfo, err := m.api.GetInstance(context.Background(), newInstance.ID)
-			if err != nil {
-				return vmInfo, err
-			}
+// 			instanceInfo, err := m.api.GetInstance(context.Background(), newInstance.ID)
+// 			if err != nil {
+// 				return vmInfo, err
+// 			}
 
-			status = instanceInfo.Status
-		}
+// 			status = instanceInfo.Status
+// 		}
 
-		// now resize it
-		resizeOptions := linodego.InstanceResizeOptions{
-			Type: "g6-standard-1",
-		}
-		err = m.api.ResizeInstance(context.Background(), newInstance.ID, resizeOptions)
-		if err != nil {
-			return vmInfo, err
-		}
+// 		// now resize it
+// 		resizeOptions := linodego.InstanceResizeOptions{
+// 			Type: "g6-standard-1",
+// 		}
+// 		err = m.api.ResizeInstance(context.Background(), newInstance.ID, resizeOptions)
+// 		if err != nil {
+// 			return vmInfo, err
+// 		}
 
-	}
+// 	}
 
-	return vmInfo, nil
-}
+// 	return vmInfo, nil
+// }
