01778535b8d4da77dbacb45e280285c97f8806b0
Athir Nuaimi <athir@nuaimi.com>
2021-03-16 21:31:57 -0400

    add details on vm sizes for each provider

--- data/fetch_cloud_details.sh
+++ data/fetch_cloud_details.sh
@@ -4,7 +4,7 @@
 
 
 # get digitalocean details
-if [ -z ${DO_API_KEY}]; then
+if [ -z ${DO_API_KEY} ]; then
   echo "DO_API_KEY is not set"
 else
   echo "getting info from digitalocean"
@@ -12,9 +12,9 @@ else
   curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DO_API_KEY" \
   "$DO_API_URL/images?page=1&per_page=500" | json_pp > digitalocean-images.json   
   curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DO_API_KEY" \
-  "$DO_API_URL/regions" | json_pp > digitalocean-regions.json
+  "$DO_API_URL/regions?page=1&per_page=100" | json_pp > digitalocean-regions.json
   curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DO_API_KEY" \
-  "$DO_API_URL/sizes" | json_pp > digitalocean-sizes.json
+  "$DO_API_URL/sizes?page=1&per_page=200" | json_pp > digitalocean-sizes.json
 
 fi
 
