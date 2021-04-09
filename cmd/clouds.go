062606cd173a9f745e54b57d4a8fa30f59b77967
Athir Nuaimi <athir@nuaimi.com>
2021-04-09 16:20:24 +0000

    fix crash in processing github releases

--- cmd/clouds.go
+++ cmd/clouds.go
@@ -39,9 +39,9 @@ var listCloudsCmd = &cobra.Command{
 		// get the subcommmand
 		clouds := []string{"digitalocean", "linode", "vultr"}
 		for _, cloud := range clouds {
-			manager, err := GetManager(cloud)
+			_, err := GetManager(cloud)
 			if err == nil {
-					fmt.Println("  ", cloud)
+				fmt.Println("  ", cloud)
 			} else {
 				fmt.Println("  ", cloud, " (not configured)")
 			}
