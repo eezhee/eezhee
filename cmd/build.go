395742ea6c5ffbe5e8dcc335d02c06ada05f5cca
Athir Nuaimi <athir@nuaimi.com>
2021-05-25 14:07:29 -0400

    make getCurrentDir work on windows - v2

--- cmd/build.go
+++ cmd/build.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strconv"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -290,8 +290,7 @@ func buildClusterName() (string, error) {
 func getCurrentDir() (string, error) {
 
 	fullDirPath, _ := os.Getwd()
-	fields := strings.Split(fullDirPath, strconv.QuoteRune(os.PathSeparator))
-	currentDir := fields[len(fields)-1]
+	currentDir := filepath.Base(fullDirPath)
 
 	return currentDir, nil
 }
